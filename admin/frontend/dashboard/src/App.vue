f3e0811559b0421b525638da1631cd48379e78fb
siduck <siduck@tutanota.com>
2026-08-16 16:05:21 +0530

    refactor(ui): use frappe-ui dialog for signedOutDialog component

--- admin/frontend/dashboard/src/App.vue
+++ admin/frontend/dashboard/src/App.vue
@@ -1,25 +1,33 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useColorScheme, FrappeUIProvider } from 'frappe-ui'
+
+import ReconnectOverlay from '@/components/common/ReconnectOverlay.vue'
+import SignedOutDialog from '@/components/common/SignedOutDialog.vue'
+import MainLayout from '@/layouts/MainLayout.vue'
+
+import { useSetupHandoff } from '@/composables/setup/useSetupHandoff'
+import { useSignedOut } from '@/composables/auth/useSignedOut'
+
+const route = useRoute()
+const isFullScreen = computed(() => route.meta.fullScreen === true)
+
+const { awaitingTerminal } = useSetupHandoff()
+const { signedOut } = useSignedOut()
+
+useColorScheme()
+</script>
+
 <template>
   <FrappeUIProvider>
     <ReconnectOverlay :paused="awaitingTerminal" />
-    <SignedOutDialog />
+    <SignedOutDialog v-if="signedOut" />
+
     <RouterView v-if="isFullScreen" />
+
     <MainLayout v-else>
       <RouterView />
     </MainLayout>
   </FrappeUIProvider>
 </template>
-
-<script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-import { useColorScheme, FrappeUIProvider } from 'frappe-ui'
-import ReconnectOverlay from './components/common/ReconnectOverlay.vue'
-import SignedOutDialog from './components/common/SignedOutDialog.vue'
-import MainLayout from './layouts/MainLayout.vue'
-import { useSetupHandoff } from './composables/setup/useSetupHandoff'
-
-const route = useRoute()
-const isFullScreen = computed(() => route.meta.fullScreen === true)
-const { awaitingTerminal } = useSetupHandoff()
-useColorScheme()
-</script>
