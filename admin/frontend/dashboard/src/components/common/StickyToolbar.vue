973a8629be2cf02ab382cd9de7c6a47a600e8fd8
siduck <siduck@tutanota.com>
2026-08-18 15:49:02 +0530

    refactor(ui): Remove useless negative margin classes in stickytoolbar component

--- admin/frontend/dashboard/src/components/common/StickyToolbar.vue
+++ admin/frontend/dashboard/src/components/common/StickyToolbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="top-12 z-10 sticky bg-surface-base -mx-3 sm:-mx-4 px-3 sm:px-4 py-2 sm:py-3">
+  <div class="top-12 z-10 sticky bg-surface-base py-2 md:py-3">
     <slot />
   </div>
 </template>
