95fb35058ef01b36655d0c90381df95bd5cd9dff
siduck <siduck@tutanota.com>
2026-08-26 15:31:58 +0530

    feat(ui): StickyToolbar add nice blur on the edges

--- admin/frontend/dashboard/src/components/common/StickyToolbar.vue
+++ admin/frontend/dashboard/src/components/common/StickyToolbar.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="top-12 z-10 sticky bg-surface-base/80 backdrop-blur">
+  <div class="top-12 z-10 sticky bg-surface-base">
     <slot />
   </div>
 </template>
+
+<style scoped>
+div::after {
+  content: "";
+  background: linear-gradient(to bottom, var(--surface-base), transparent);
+  @apply top-full absolute inset-x-0 h-4 pointer-events-none;
+}
+</style>
