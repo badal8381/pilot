86779241b952c95cd8974949fee6e915981005a8
siduck <siduck@tutanota.com>
2026-08-31 10:58:54 +0530

    refactor(ui): drop dayjs for Intl.RelativeTimeFormat

--- admin/frontend/dashboard/src/utils/time.ts
+++ admin/frontend/dashboard/src/utils/time.ts
@@ -1,3 +1,18 @@
-import { dayjs } from 'frappe-ui'
+const UNITS: [Intl.RelativeTimeFormatUnit, number][] = [
+  ['year', 31536000],
+  ['month', 2592000],
+  ['week', 604800],
+  ['day', 86400],
+  ['hour', 3600],
+  ['minute', 60],
+  ['second', 1],
+]
 
-export const relativeTime = (value) => dayjs(value).fromNow()
+const formatter = new Intl.RelativeTimeFormat(undefined, { numeric: 'auto' })
+
+export const relativeTime = (value) => {
+  const seconds = (new Date(value).getTime() - Date.now()) / 1000
+  const [unit, size] = UNITS.find(([, size]) => Math.abs(seconds) >= size) ?? UNITS.at(-1)
+
+  return formatter.format(Math.round(seconds / size), unit)
+}
